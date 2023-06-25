4b4494c59bafb2d775538184103574fad6fedfad
Neko <artyem.popov.v@gmail.com>
2023-06-25 21:27:21 +0200

    modified:   commands/user/avatarContext.js 	modified:   main.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/ascii-table/.jshintrc 	new file:   node_modules/ascii-table/.npmignore 	new file:   node_modules/ascii-table/.travis.yml 	new file:   node_modules/ascii-table/Makefile 	new file:   node_modules/ascii-table/ascii-table.js 	new file:   node_modules/ascii-table/ascii-table.min.js 	new file:   node_modules/ascii-table/bower.json 	new file:   node_modules/ascii-table/example/simple.js 	new file:   node_modules/ascii-table/example/simple.txt 	new file:   node_modules/ascii-table/index.js 	new file:   node_modules/ascii-table/license 	new file:   node_modules/ascii-table/package.json 	new file:   node_modules/ascii-table/readme.md 	new file:   node_modules/ascii-table/test.js 	modified:   package-lock.json 	modified:   package.json

--- commands/user/avatarContext.js
+++ commands/user/avatarContext.js
@@ -1,23 +1,26 @@
-const { EmbedBuilder, SlashCommandBuilder, ContextMenuCommandBuilder } = require("discord.js");
+const { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
-    data: new ContextMenuCommandBuilder()
-        .setName('avatar'),
+  data: new ContextMenuCommandBuilder()
+    .setName("Avatar")
+    .setType(ApplicationCommandType.User),
 
-        async execute (interaction) {
+  async execute(interaction) {
 
-            let user = interaction.options.getUser('user') || interaction.user;
-            let userAvatar = user.displayAvatarURL({ ditamic: true, size: 4096})
+    let user = interaction.options.getUser('user') || interaction.user;
+    let userAvatar = user.displayAvatarURL({ dinamic: true, size: 4096 })
 
-            const embed = new EmbedBuilder()
-                .setColor(0x74c7ec)
-                .setAuthor({ name: `Аватар пользователя: ${user.username} ` , url: userAvatar, })
-                .setFooter({text: `Вызвал: ${interaction.user.username}`,
-                            iconURL: interaction.user.displayAvatarURL({ ditamic: true, size: 4096 })})
-                .setTimestamp()
-                .setImage(`${userAvatar}`);
+    const embed = new EmbedBuilder()
+    .setColor(0x74c7ec)
+    .setAuthor({ name: `Аватар пользователя: ${user.username} ` , url: userAvatar, })
+    .setFooter({text: `Вызвал: ${interaction.user.username}`,
+                iconURL: userAvatar})
+    .setTimestamp()
+    .setImage(`${userAvatar}`);
 
-                await interaction.reply({
-                    embeds: [embed],});
-    },
+
+    await interaction.reply({
+      embeds: [embed],
+    });
+  },
 };
--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+    console.log(`Запущенa ${c.user.tag}`);
 });
 
 
@@ -29,24 +29,21 @@ for (const folder of commandFolders) {
 		}
 	}
 }
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
+client.on(Events.InteractionCreate, async(interaction,client) => {
+    if (!interaction.isChatInputCommand()) {
+        const command = interaction.client.commands.get(interaction.commandName);
         await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+
+    } else if (interaction.isContextMenuCommand()) {
+        const contextCommand = client.commands.get(interaction.commandName);
+        
+        if(!contextCommand) return;
+
+        try {
+            await contextCommand.execute(interaction, client);
+        } catch (error) {
+            console.error(error);
+            return
         }
     }
 });
