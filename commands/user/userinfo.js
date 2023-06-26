cf6f118d447496c6b6db2b140162cf86115a2cd2
Neko <artyem.popov.v@gmail.com>
2023-06-26 23:21:07 +0200

    modified:   commands/user/userinfo.js

--- commands/user/userinfo.js
+++ commands/user/userinfo.js
@@ -21,20 +21,22 @@ module.exports = {
             let badges = [];
 
             await Promise.all (flags.map( async badge => {
-                if (badge === 'Staff') badges.push('Discord');
-                if (badge === 'Partner') badges.push('Discord');
+                if (badge === 'Staff') badges.push('<:discordstaff:1122923347651600507>');
+                if (badge === 'Partner') badges.push('<:discordpartner:1122923351279673406>');
                 if (badge === 'DISCORD_EMPLOYEE') badges.push('Discord');
-                if (badge === 'Hypesquad') badges.push('Discord');
+                if (badge === 'Hypesquad') badges.push('<:hypesquadevents:1122923339539808377>');
                 if (badge === 'HypeSquadOnlineHouse1') badges.push('<:hypesquadbravery:1122923344862380092>');
                 if (badge === 'HypeSquadOnlineHouse2') badges.push('<:hypesquadbrilliance:1122923342614233149>');
                 if (badge === 'HypeSquadOnlineHouse3') badges.push('<:hypesquadbalance:1122923346124877956>');
-                if (badge === 'BugHunterLevel1') badges.push('Discord');
-                if (badge === 'BugHunterLevel2') badges.push('Discord');
-                if (badge === 'ActiveDeveloper') badges.push('Discord');
-                if (badge === 'VerifiedDeveloper') badges.push('Discord');
-                if (badge === 'PremiumEarlySupporter') badges.push('Discord');
-                if (badge === 'VerifiedBot') badges.push('Discord');
+                if (badge === 'BugHunterLevel1') badges.push('<:discordbughunter1:1122923362134532136>');
+                if (badge === 'BugHunterLevel2') badges.push('<:discordbughunter2:1122923359886376960>');
+                if (badge === 'ActiveDeveloper') badges.push('<:activedeveloper:1122923369734606888>');
+                if (badge === 'VerifiedDeveloper') badges.push('<:discordbotdev:1122923364441391135> ');
+                if (badge === 'PremiumEarlySupporter') badges.push('<:discordearlysupporter:1122923357042659348>');
+                if (badge === 'VerifiedBot') badges.push('<:activedeveloper:1122923369734606888>');
             }));
+
+            if (user.tag === `${user.username}#0`) badges.push('<:Knownas:1122999154663243816>');
 
             const embed = new EmbedBuilder()
                 .setColor(0x74c7ec)
@@ -43,13 +45,10 @@ module.exports = {
                 .setURL(`${userAvatar}`)
                 .setThumbnail(`${userAvatar}`)
                 .addFields(
-                    { name: 'Псевдоним:', value: `${user}` },
-                    { name: 'Значки: ', value: `${badges.join('') || '**Нема таковых(**'}`, inline: true },
-                    { name: '\u200B', value: '\u200B' },
+                    { name: 'Псевдоним:', value: `${user} ${badges.join('') || ''}`, inline: false },
+                    // { name: 'Бейджики: ', value: ``, inline: true },
                     { name: 'Участник c:', value: `<t:${parseInt(member.joinedAt / 1000)}:F>`, inline: true },
-                    { name: 'В дс с:', value: `<t:${parseInt(user.createdAt / 1000)}:F>`, inline: true },
-//                    { name: '\u200B', value: '\u200B' },
-//                    { name: `111`,value: `${user.bot}`, inline: true },
+                    { name: 'Зарегался:', value: `<t:${parseInt(user.createdAt / 1000)}:R>`, inline: true },
                 )
                 .setFooter({text: `Вызвал: ${interaction.user.username}`,
                             iconURL: interaction.user.displayAvatarURL({ ditamic: true, size: 32 })})
@@ -60,11 +59,11 @@ module.exports = {
             const embedBot = new EmbedBuilder()
                 .setColor(0x74c7ec)
                 .setAuthor({ name: `Информация о Боте`, url: userAvatar, })
-                .setTitle(`${user.username} ${badges.join('')}`)
+                .setTitle(`${user.username}`)
                 .setURL(`${userAvatar}`)
                 .setThumbnail(`${userAvatar}`)
                 .addFields(
-                    { name: 'Имя:', value: `${user}` },
+                    { name: 'Имя:', value: `${user} ${badges.join('')}` },
                     { name: 'Добавлен в:', value: `<t:${parseInt(member.joinedAt / 1000)}:F>`, inline: true },
                     { name: 'Создан:', value: `<t:${parseInt(user.createdAt / 1000)}:R>`, inline: true },
                 )
